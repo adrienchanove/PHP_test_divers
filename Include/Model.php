d9f2dd47e034a4b1a3e01718ed8aaf106eebae54
achanove <adrien.chanove@gmail.com>
2024-02-01 16:20:53 +0100

    Update Model.php
    Je transforme la class en interface

--- Include/Model.php
+++ Include/Model.php
@@ -4,43 +4,37 @@
  * Model
  */
 
-class Model
+ interface   Model
 {
-    static function getById($id)
-    {
-        $table = get_called_class();
-        $sql = "SELECT * FROM $table WHERE id = $id";
-        $bdd = new Bdd();
-        $result = $bdd->execute($sql);
-        $all = $result->fetch(PDO::FETCH_ASSOC);
-        $listeInstance = [];
-        foreach ( $all as $entite ) {
-            $listeInstance[] = self::arrayToEntity($entite);
-        }
-        return $listeInstance;
-    }
+    /**
+     * Get a record by id
+     * @param int $id
+     * @return Model
+     */
+    static function getById($id);
+    
+    /**
+     * Get all records
+     * @return Model[]
+     */
+    static function getAll();
 
-    static function getAll()
-    {
-        $table = get_called_class();
-        $sql = "SELECT * FROM $table";
-        $bdd = new Bdd();
-        $result = $bdd->execute($sql);
-        $all = $result->fetchAll(PDO::FETCH_ASSOC);
-        $listeInstance = [];
-        foreach ( $all as $entite ) {
-            $listeInstance[] = self::arrayToEntity($entite);
-        }
-        return $listeInstance;
-    }
+    /**
+     * Array to entity
+     * Process array to entity
+     * @param array $array
+     */
+    function arrayToEntity($array);
 
-    static private function arrayToEntity($array)
-    {
-        $table = get_called_class();
-        $instance = new $table();
-        foreach ( $array as $key => $value ) {
-            $instance->$key = $value;
-        }
-        return $instance;
-    }
+    /**
+     * Save instance in database
+     */
+    public function save();
+
+    /**
+     * lastInsertId
+     * @return int
+     */
+    static function lastInsertId();
+    
 }
